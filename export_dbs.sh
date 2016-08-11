b0afe146adf99cdbccf518920b373c02b20f6dc5
Antonio Jiménez Mavillard <ajimene6@uwo.ca>
2016-08-10 23:14:51 -0400

    Parsers done

--- export_dbs.sh
+++ export_dbs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # elbulli
+# json
 echo "Exporting to 'recipes_elbulli_raw.json'..."
 mongoexport --db recipes --collection elbulli_raw --out data/dbs/recipes_elbulli_raw.json
 echo
@@ -16,6 +17,7 @@ echo
 echo "Exporting to 'recipes_elbulli_cuisines.json'..."
 mongoexport --db recipes --collection elbulli_cuisines --out data/dbs/recipes_elbulli_cuisines.json
 echo
+# csv
 echo "Exporting to 'recipes_elbulli_raw.csv'..."
 mongoexport --db recipes --collection elbulli_raw --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_elbulli_raw.csv
 echo
@@ -30,8 +32,9 @@ mongoexport --db recipes --collection elbulli_cuisines --fields=_id,title,year,i
 echo
 echo "Exporting to 'recipes_elbulli_cuisines.csv'..."
 mongoexport --db recipes --collection elbulli_types --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_elbulli_cuisines.csv
-# cookpad
 echo
+# cookpad
+# json
 echo "Exporting to 'recipes_cookpad_raw.json'..."
 mongoexport --db recipes --collection cookpad_raw --out data/dbs/recipes_cookpad_raw.json
 echo
@@ -47,6 +50,7 @@ echo
 echo "Exporting to 'recipes_cookpad_cuisines.json'..."
 mongoexport --db recipes --collection cookpad_cuisines --out data/dbs/recipes_cookpad_cuisines.json
 echo
+# csv
 echo "Exporting to 'recipes_cookpad_raw.csv'..."
 mongoexport --db recipes --collection cookpad_raw --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_cookpad_raw.csv
 echo
@@ -61,5 +65,35 @@ mongoexport --db recipes --collection cookpad_cuisines --fields=_id,title,year,i
 echo
 echo "Exporting to 'recipes_cookpad_cuisines.csv'..."
 mongoexport --db recipes --collection cookpad_types --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_cookpad_cuisines.csv
+echo
+# allrecipes
+# json
+echo "Exporting to 'recipes_allrecipes_raw.json'..."
+mongoexport --db recipes --collection allrecipes_raw --out data/dbs/recipes_allrecipes_raw.json
+echo
+echo "Exporting to 'recipes_allrecipes_representatives.json'..."
+mongoexport --db recipes --collection allrecipes_representatives --out data/dbs/recipes_allrecipes_representatives.json
+echo
+# csv
+echo "Exporting to 'recipes_allrecipes_raw.csv'..."
+mongoexport --db recipes --collection allrecipes_raw --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_allrecipes_raw.csv
+echo
+echo "Exporting to 'recipes_allrecipes_representatives.csv'..."
+mongoexport --db recipes --collection allrecipes_representatives --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_allrecipes_representatives.csv
+echo
+# epicurious
+# json
+echo "Exporting to 'recipes_epicurious_raw.json'..."
+mongoexport --db recipes --collection epicurious_raw --out data/dbs/recipes_epicurious_raw.json
+echo
+echo "Exporting to 'recipes_epicurious_representatives.json'..."
+mongoexport --db recipes --collection epicurious_representatives --out data/dbs/recipes_epicurious_representatives.json
+echo
+# csv
+echo "Exporting to 'recipes_epicurious_raw.csv'..."
+mongoexport --db recipes --collection epicurious_raw --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_epicurious_raw.csv
+echo
+echo "Exporting to 'recipes_epicurious_representatives.csv'..."
+mongoexport --db recipes --collection epicurious_representatives --fields=_id,title,year,ingredients,techniques --type=csv --out data/dbs/recipes_epicurious_representatives.csv
 echo
 echo "Done."
